00f517e589b03399002d8b6e55d24e53e7c2dd1b
Christina Schweipert <c.schweipert@gmail.com>
2021-03-23 13:05:13 -0600

    step1 and step2

--- step2.js
+++ step2.js
@@ -1,18 +1,23 @@
 const axios = require("axios");
+const cat = require("./step1.js")
 
 const fsP = require('fs/promises');
 const argv = process.argv;
 
 async function webCat(path) {
     let stringPath = path.toString()
+
     try {
         let contents = await axios.get(stringPath);
-        fsP.readFile(contents.data, "utf8");
-        console.log("file contents", contents);
+        console.log("file contents", contents.data);
     } catch (err) {
         console.log(err)
         process.exit(1);
     }
 }
 
-webCat(argv[2]);
+let path = process.argv[2];
+
+let resultPromise = path.startsWith("http")
+    ? webCat(path)
+    : cat(path);
